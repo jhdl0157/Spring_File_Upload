9e0b40f7e01b209a7abc6f0dc5a8d5035f23c082
jhdl0157 <jhdl0157@naver.com>
2022-09-06 13:25:41 +0900

    win, mac 파일위치 자동으로 변경

--- src/main/java/com/ll/exam/app10/fileupload/controller/FileUploadController.java
+++ src/main/java/com/ll/exam/app10/fileupload/controller/FileUploadController.java
@@ -13,19 +13,28 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/upload")
 public class FileUploadController {
-    @Value("${custom.genFileDirPath}")
-    private String genFileDirPath;
+    @Value("${custom.genWinFileDirPath}")
+    private static String genWinFileDirPath;
+    @Value("${custom.genMacFileDirPath}")
+    private static  String genMacFileDirPath;
 
     @RequestMapping("")
     @ResponseBody
     public String upload(@RequestParam("img1") MultipartFile img1, @RequestParam("img2") MultipartFile img2) {
         try {
-            img1.transferTo(new File(genFileDirPath + "/1.png"));
-            img2.transferTo(new File(genFileDirPath + "/2.png"));
+            img1.transferTo(new File(getOsInfo() + "/1.png"));
+            img2.transferTo(new File(getOsInfo() + "/2.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         return "업로드 완료!";
+    }
+
+    private static String getOsInfo(){
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return genWinFileDirPath;
+        }
+        return genMacFileDirPath;
     }
 }
