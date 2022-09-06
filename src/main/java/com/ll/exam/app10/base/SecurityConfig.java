774fa2e9923dbf1d35a66356f731a7da9b86a8e3
jhdl0157 <jhdl0157@naver.com>
2022-09-06 13:46:32 +0900

    csrf token disable

--- src/main/java/com/ll/exam/app10/base/SecurityConfig.java
+++ src/main/java/com/ll/exam/app10/base/SecurityConfig.java
@@ -1,17 +1,30 @@
 package com.ll.exam.app10.base;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                    .csrf().disable();
         return http.build();
     }
+
+
+
+
 }
