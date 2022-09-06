f49e0c1f71ab7dfbb4684ef37005b13c5fc723e6
jhdl0157 <jhdl0157@naver.com>
2022-09-06 13:47:16 +0900

    ResourceHandlers add

--- src/main/java/com/ll/exam/app10/fileupload/controller/FileUploadController.java
+++ src/main/java/com/ll/exam/app10/fileupload/controller/FileUploadController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app10.fileupload.controller;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,28 +14,37 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/upload")
 public class FileUploadController {
-    @Value("${custom.genWinFileDirPath}")
-    private static String genWinFileDirPath;
-    @Value("${custom.genMacFileDirPath}")
-    private static  String genMacFileDirPath;
+    @Value("${custom.genFileDirPath}")
+    private  String DIR_URL;
+
+    @Value("${custom.genMac}")
+    private  String MAC_DIR_URL;
+
 
     @RequestMapping("")
     @ResponseBody
     public String upload(@RequestParam("img1") MultipartFile img1, @RequestParam("img2") MultipartFile img2) {
         try {
-            img1.transferTo(new File(getOsInfo() + "/1.png"));
-            img2.transferTo(new File(getOsInfo() + "/2.png"));
+            System.out.println(getOsDir());
+            img1.transferTo(new File(getOsDir()+"/1.png"));
+            img2.transferTo(new File(getOsDir() + "/2.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return "업로드 완료!";
     }
 
-    private static String getOsInfo(){
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("win")) {
-            return genWinFileDirPath;
+    public  String getOsDir(){
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("win")) {
+            System.out.println("This is Windows");
+            return DIR_URL;
+        } else if (osName.contains("mac")) {
+            System.out.println("This is Mac");
+            return MAC_DIR_URL;
         }
-        return genMacFileDirPath;
+        return null;
     }
+
+
 }
